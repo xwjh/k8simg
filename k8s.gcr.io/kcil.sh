f94e94c701370d8f17fe86827c7ab543ba8db1d0
xwjh <xwjh@outlook.com>
2021-07-27 03:38:44 -0400

    v1.21.3

---
+++ k8s.gcr.io/kcil.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+kubeadm config images list > kcil.txt
+cat kcil.txt | awk -F '[/:]' '{print "mkdir "$2"> /dev/null 2>&1;echo -e \"from "$0"\\nlabel maintainer=\\\"xwjh@outlook.com\\\"\" > "$2"/Dockerfile"}'|bash
+
+cd ..
+TAG=$(kubeadm version -ojson|jq -r .clientVersion.gitVersion)
+git tag -a $TAG -m $TAG
+git add -A
+git commit -m $TAG
+git push origin --tags $TAG
+git push origin master
+
