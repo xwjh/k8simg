402ce2bc1f48553734da927f45a2af5f9dfe414b
xwjh <xwjh@outlook.com>
2021-08-09 15:12:08 +0800

    v1.22.0

--- bin/kcil.sh
+++ bin/kcil.sh
@@ -3,9 +3,8 @@
 cd `dirname $0`
 
 kubeadm config images list > ../k8s.gcr.io/kcil.txt
-cat kcil.txt | awk -F '[/:]' '{print "mkdir "$2" > /dev/null 2>&1; echo -e \"from "$0"\" > "$2"/Dockerfile"}'|bash
+cat ../k8s.gcr.io/kcil.txt | awk -F '[/:]' '{print "mkdir "$2" > /dev/null 2>&1; echo -e \"from "$0"\" > "$2"/Dockerfile"}'|bash
 
-cd ..
 TAG=$(kubeadm version -ojson|jq -r .clientVersion.gitVersion)
 git add -A
 git commit -m $TAG
